b26c1ce2c6d21222577b5e9335c4b0ac7b0bc7e0
Arshaan <arshaan.bhimani@uwaterloo.ca>
2021-11-10 15:54:11 -0500

    updated home page copy, set up email services

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,15 +1,45 @@
-import * as React from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+
+// import * as React from 'react';
 
 function HomeScreen() {
   return (
     <div>
-      <p>You can earn 20%, 50% or 150% on your dollars every year, here's how!</p>
-      <p>Step 1</p>
-      <p>Deposit your funds!</p>
-      <p>Step 2</p>
-      <p>Browse our collection of investments and select one that fits your risk profile best!</p>
-      <p>Step 3</p>
-      <p>Check back every day and see your investment grow.</p>
+      <p
+        css={{
+          fontSize: '22px',
+        }}
+      >
+        You can earn 20%, 50% or 150% on your dollars every year, here's how!
+      </p>
+      <p>
+        <span css={{ fontSize: '20px', marginRight: '10px' }}>Step 1</span> Deposit your funds.
+      </p>
+      <p>
+        <span css={{ fontSize: '20px', marginRight: '10px' }}>Step 2</span> Browse our collection of
+        investments and select one that fits your risk profile best.
+      </p>
+      <p>
+        <span css={{ fontSize: '20px', marginRight: '10px' }}>Step 3</span> Check back every day and
+        see your investment grow.
+      </p>
+      <hr />
+      <p>
+        Click the bright green "Getting Started" button at the top of the page to set up your
+        account.
+      </p>
+      <hr />
+      <p
+        css={{
+          fontSize: '22px',
+        }}
+      >
+        FAQ (Coming Soon!)
+      </p>
+      <p>
+        Email questions@cropfarming.org if you have any urgent questions or want to get in touch!
+      </p>
     </div>
   );
 }
